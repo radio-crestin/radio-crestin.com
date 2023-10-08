c2586ec42b49f00eb93cdefbbd776074b3bf015d
Elisei.Nicolae <nicolae.elisei1999@gmail.com>
2023-10-08 21:25:01 +0300

    fix: config CORS to allow radiocrestin.ro to make requests.

--- pages/api/v1/review.ts
+++ pages/api/v1/review.ts
@@ -2,43 +2,75 @@ import {NextApiRequest, NextApiResponse} from "next";
 import {Review} from "../../../types";
 import {postReview} from "../../../backendServices/review";
 import { v4 as uuidv4 } from 'uuid';
+import Cors from 'cors';
+
+// Define a Session type
+interface Session {
+  ip: string;
+  id: string;
+}
+
+// Extend NextApiRequest to include session of type Session
+interface ExtendedApiRequest extends NextApiRequest {
+  session: Session;
+}
+
+// Define type for the review response
+interface ReviewResponse {
+  done: boolean;
+}
+
+// Configure CORS
+const cors = Cors({
+  methods: ['POST'],
+  origin: [
+    'http://localhost:3000',
+    'https://radio-crestin.com',
+    'https://radiocrestin.ro',
+  ],
+});
+
+function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: Function): Promise<void> {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
 
 export default async function handler(
-  req: NextApiRequest & {session: any},
-  res: NextApiResponse<{done: boolean}>,
-) {
-  if (
-    typeof req.session === 'undefined' ||
-    typeof req.session.ip === 'undefined' ||
-    typeof req.session.id === 'undefined'
-  ) {
+  req: ExtendedApiRequest,
+  res: NextApiResponse<ReviewResponse>,
+): Promise<void> {
+  await runMiddleware(req, res, cors);
+
+  if (!req.session || !req.session.ip || !req.session.id) {
     Object.defineProperty(req, 'session', {
       value: {
-        ip:
-          (req.headers['x-forwarded-for'] as string)?.split(',')[0]?.trim() ||
-          req.socket.remoteAddress ||
-          '',
+        ip: (req.headers['x-forwarded-for'] as string)?.split(',')[0]?.trim() || req.socket.remoteAddress || '',
         id: uuidv4(),
       },
       writable: true,
     });
   }
-  // TODO: we might need to implement a protection mechanism here..
 
   const review: Review = {
-    user_name: req.query.user_name || req.body.user_name,
+    user_name: req.query.user_name as string || req.body.user_name,
     ip_address: req.session.ip,
     session_id: req.session.id,
-    station_id: req.query.station_id || req.body.station_id,
-    stars: req.query.stars || req.body.stars,
-    message: req.query.message || req.body.message,
+    station_id: req.query.station_id as string || req.body.station_id,
+    stars: Number(req.query.stars) || req.body.stars,
+    message: req.query.message as string || req.body.message,
   };
-  return postReview(review)
-    .then(response => {
-      res.status(200).json({done: response.done});
-    })
-    .catch(error => {
-      console.error('error', error);
-      res.status(500).json({done: false});
-    });
+
+  try {
+    const response = await postReview(review);
+    res.status(200).json({done: response.done});
+  } catch (error) {
+    console.error('error', error);
+    res.status(500).json({done: false});
+  }
 }
